d9a69227f00a7a7c66854a3938f30c8025c7197d
QuickGo <zymantas.rastenis@gmail.com>
2024-04-03 19:20:42 +0300

    Fix: function DataReady fixes

--- Turbo-bot/TradingAlgo/ConvertCSV.py
+++ Turbo-bot/TradingAlgo/ConvertCSV.py
@@ -1,10 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv("/home/zymantas/Desktop/Training_data/5minData/ATOMUSDT5min.csv")
-
 def DataReady(DataFrame):
-    DataFrame.rename(columns = {'datetime' : 'timestamp'}, inplace = True)
     DataFrame.drop('symbol', axis = 1, inplace = True)
-    DataFrame
-    print(DataFrame)
+    DataFrame.index = pd.to_datetime(DataFrame.index)
     return DataFrame

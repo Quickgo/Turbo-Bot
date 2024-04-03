0d05e9db401a17de7987ae178abc6c0c817aea43
QuickGo <zymantas.rastenis@gmail.com>
2024-04-03 19:17:39 +0300

    Remove: runs file

--- Turbo-bot/TradingAlgo/ConvertCSV.py
+++ Turbo-bot/TradingAlgo/ConvertCSV.py
@@ -5,5 +5,6 @@ df = pd.read_csv("/home/zymantas/Desktop/Training_data/5minData/ATOMUSDT5min.csv
 def DataReady(DataFrame):
     DataFrame.rename(columns = {'datetime' : 'timestamp'}, inplace = True)
     DataFrame.drop('symbol', axis = 1, inplace = True)
+    DataFrame
     print(DataFrame)
     return DataFrame

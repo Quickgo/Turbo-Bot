c59cc5c43cc3b21fff50f826e8874dfd85b74cb9
QuickGo <zymantas.rastenis@gmail.com>
2024-04-24 07:15:45 +0300

    Add: Added features for agent to train on

--- Turbo-bot/TradingAlgo/ConvertCSV.py
+++ Turbo-bot/TradingAlgo/ConvertCSV.py
@@ -2,5 +2,6 @@ import pandas as pd
 
 def DataReady(DataFrame):
     DataFrame.drop('symbol', axis = 1, inplace = True)
-    DataFrame.index = pd.to_datetime(DataFrame.index)
+    DataFrame.index = pd.to_datetime(DataFrame.datetime)
+    DataFrame.drop('datetime', axis = 1, inplace = True)
     return DataFrame

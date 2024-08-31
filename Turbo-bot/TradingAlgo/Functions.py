8ea807521430d54de920fa54edcf7b0513255f75
QuickGo <zymantas.rastenis@gmail.com>
2024-08-31 13:25:50 +0300

    Add: Added downloading data straight into the trading algo. To get big enough data, for my testing purposes i will be chaining months of data into one csv file

--- Turbo-bot/TradingAlgo/Functions.py
+++ Turbo-bot/TradingAlgo/Functions.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 
+from tvDatafeed import TvDatafeed, Interval
+
 def DataReady(DataFrame):
     DataFrame.drop('symbol', axis = 1, inplace = True)
     DataFrame.index = pd.to_datetime(DataFrame.datetime)
@@ -12,3 +14,8 @@ def KijunSen(data, length):
     lowest_low = pd.Series(data["Low"].rolling(window=length).min())
     highest_high = pd.Series(data["High"].rolling(window=length).max())
     return (lowest_low + highest_high) / 2
+
+def GetData():
+    tv = TvDatafeed()
+    Data = tv.get_hist("ATOMUSDT.P", "BYBIT", interval=Interval.in_5_minute, n_bars=50000, extended_session=True)
+    Data.to_csv("/home/porsche/Desktop/Training_data/5minData/ATOMUSDT5min.csv")
